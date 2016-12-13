4e2ad26956686d84d62d34e4210e5d9dad08151a
Shariq Iqbal <shariqiqbal2810@users.noreply.github.com>
2016-12-13 10:59:34 -0500

    Moved get_network convenience function to separate utils.py file

--- code/DLGM.py
+++ code/DLGM.py
@@ -1,52 +1,8 @@
 import lasagne
 import theano.tensor as T
 from layers import *
-from lasagne.nonlinearities import rectify, linear, tanh
-
-def get_network(input_dim, output_dim, hidden_dim, num_layers,
-                PKLparams, srng, batchnorm=False, is_shooter=False,
-                row_sparse=False, add_pklayers=False, filt_size=None):
-    """
-    Returns a NN with the specified parameters.
-    Also returns a list of PKBias layers
-    """
-    PKbias_layers = []
-    NN = lasagne.layers.InputLayer((None, input_dim))
-    if batchnorm:
-        NN = lasagne.layers.BatchNormLayer(NN)
-    if filt_size is not None:  # first layer convolution
-        # rearrange dims for convolution
-        NN = lasagne.layers.DimshuffleLayer(NN, ('x', 1, 0))
-        # custom pad so that no timepoint gets input from future
-        NN = lasagne.layers.PadLayer(NN, [(0, 0), (filt_size - 1, 0)], batch_ndim=1)
-        # Perform convolution (no pad, no filter flip (for interpretability))
-        NN = lasagne.layers.Conv1DLayer(NN, num_filters=hidden_dim, filter_size=filt_size,
-                                        pad='valid', flip_filters=False,
-                                        nonlinearity=rectify)
-        # rearrange dims for dense layers
-        NN = lasagne.layers.DimshuffleLayer(NN, (2, 1))
-    for i in range(num_layers):
-        if is_shooter and add_pklayers:
-            if row_sparse:
-                PK_bias = PKRowBiasLayer(NN, srng, PKLparams)
-            else:
-                PK_bias = PKBiasLayer(NN, srng, PKLparams)
-            PKbias_layers.append(PK_bias)
-            NN = PK_bias
-        if i == num_layers - 1:
-            NN = lasagne.layers.DenseLayer(NN,
-                                           output_dim,
-                                           nonlinearity=linear,
-                                           W=lasagne.init.Normal(std=0.1))
-        else:
-            NN = lasagne.layers.DenseLayer(NN,
-                                           hidden_dim,
-                                           nonlinearity=rectify,
-                                           W=lasagne.init.Orthogonal())
-    if add_pklayers:
-        return NN, PKbias_layers
-    else:
-        return NN
+from lasagne.nonlinearities import rectify, linear
+from utils import get_network
 
 
 class DLGM(object):
