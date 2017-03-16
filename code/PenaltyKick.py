e3ff470ecf0dc292e74ed4c542bcc5e57c5a9de9
Shariq Iqbal <shariqiqbal2810@users.noreply.github.com>
2017-03-16 11:51:59 -0400

    Updated/added licenses to all files

--- code/PenaltyKick.py
+++ code/PenaltyKick.py
@@ -1,6 +1,6 @@
 """
 The MIT License (MIT)
-Copyright (c) 2015 Evan Archer
+Copyright (c) 2017 Shariq Iqbal
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
